98e3f0bce968bace75fe150ca1951c9f396dfefc
atan94 <amostan94@gmail.com>
2025-12-04 13:22:08 +0800

    Refactor main function in main.c to remove unused variables and comments

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+#include "ft_printf.h"
+#include <limits.h>
 #include <stdio.h>
 
 int	main(void)
 {
-	char *s = NULL;
-	printf("%s\n", s);
+	int	unknown;
+
+	// int		a;
+	// int		b;
+	// char	*s;
+	// s = NULL;
+	// a = printf("stdio: %s[%%]\n", s);
+	// b = ft_printf("mine : %s[%%]\n", s);
+	// printf("ret stdio = %d, ret mine = %d\n", a, b);
+	unknown = printf("what will this print --> %w interesting....", 42);
+	printf("%d\n", unknown);
 }
