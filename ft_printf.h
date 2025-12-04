5e955b6305c0c8f75e7c9c6b4e06b0afd2cf01c0
atan94 <amostan94@gmail.com>
2025-12-04 13:21:39 +0800

    Update header file ft_printf.h to adjust include paths

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,20 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 17:16:06 by amtan             #+#    #+#             */
-/*   Updated: 2025/11/27 18:31:06 by amtan            ###   ########.fr       */
+/*   Updated: 2025/11/29 00:02:32 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft.h"
+# include "libft/libft.h"
 # include <stdarg.h>
+# include <stddef.h>
 # include <unistd.h>
 
-int			ft_printf(const char *format, ...);
+int	ft_printf(const char *format, ...);
+int	print_char(int c, size_t *printed);
+int	print_str(char *str, size_t *printed);
 
 #endif
