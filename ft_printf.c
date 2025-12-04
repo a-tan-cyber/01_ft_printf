ffdba299a9cd52915c48fe8578034480cf67e7b8
atan94 <amostan94@gmail.com>
2025-12-04 13:21:06 +0800

    Refactor ft_printf to improve format handling and add support for additional specifiers

--- ft_printf.c
+++ ft_printf.c
@@ -6,35 +6,83 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 17:41:17 by amtan             #+#    #+#             */
-/*   Updated: 2025/11/27 18:48:11 by amtan            ###   ########.fr       */
+/*   Updated: 2025/11/29 02:37:35 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <limits.h>
+
+static int	print_next(const char **format, va_list *ap, size_t *printed);
+static int	process_specifier(const char **format, va_list *ap,
+				size_t *printed);
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
-	int		count;
+	size_t	printed;
+	int		ret;
 
+	if (!format)
+		return (-1);
 	va_start(ap, format);
-	count = 0;
+	printed = 0;
 	while (*format)
 	{
-		if (*format == '%')
+		ret = print_next(&format, &ap, &printed);
+		if (ret < 0)
 		{
-			count += handle_conversion(&format, &ap);
-			format++;
-		}
-		else
-		{
-			ft_putchar_rd(*format, 1);
-			count++;
-			format++;
+			va_end(ap);
+			return (-1);
 		}
 		format++;
 	}
 	va_end(ap);
-	return (count);
+	if (printed > (size_t)INT_MAX)
+		return (-1);
+	return (printed);
 }
 
+static int	print_next(const char **format, va_list *ap, size_t *printed)
+{
+	int	ret;
+
+	if (**format == '%')
+	{
+		if (!(*format + 1))
+			return (0);
+		(*format)++;
+		if (**format == '%')
+			ret = print_char('%', printed);
+		else
+			ret = process_specifier(format, ap, printed);
+	}
+	else
+		ret = print_char(*format, printed);
+	return (ret);
+}
+
+static int	process_specifier(const char **format, va_list *ap, size_t *printed)
+{
+	char	specifier;
+	int		ret;
+
+	specifier = **format;
+	if (specifier == 'c')
+		ret = print_char(va_arg(*ap, int), printed);
+	else if (specifier == 's')
+		ret = print_str(va_arg(*ap, char *), printed);
+	else if (specifier == 'd' || specifier == 'i')
+		ret = print_signed_base((long)va_arg(*ap, int), specifier, printed);
+	else if (specifier == 'x' || specifier == 'X' || specifier == 'u')
+		ret = print_unsigned_base((long)va_arg(*ap, unsigned int), specifier,
+				printed);
+	else if (specifier == 'p')
+		ret = print_pointer((long)va_arg(*ap, unsigned long), specifier,
+				printed);
+	else
+		ret = print_char(specifier, printed);
+	return (ret);
+}
+
+/* TODO: later handle p, d, i, u, x, X */
